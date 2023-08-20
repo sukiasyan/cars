92026051019aafbb3a730db8eb3dda5097b708e6
hsukiasyan <hakob.sukiasyan@pricespider.com>
2023-08-20 15:28:59 +0200

    first page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,29 @@
+'use client'
+
+import { CustomButton } from '@/components'
+
 const Hero = () => {
-  return <> Hero</>
+  const handleScroll = () => {
+    console.log('Function: handleScroll - Line 5 - ')
+  }
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car -- quickly and easily
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experiance with our efforless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Hero
